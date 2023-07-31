d0311c00dbd628282390a1797036c732a882e12e
Zkish <zachary.kish@gmail.com>
2023-07-31 00:24:02 -0400

    youtube downloader extra code.

--- LifeIssues/YoutubeDownloader.py
+++ LifeIssues/YoutubeDownloader.py
@@ -1,14 +1,18 @@
 from pytube import YouTube
 
-def Download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+def download_youtube_video(url, output_path):
     try:
-        youtubeObject.download()
-    except:
-        print("An error has occurred")
-    print("Download is completed successfully")
+        # Download the YouTube video
+        youtube = YouTube(url)
+        video_stream = youtube.streams.get_highest_resolution()
+        video_stream.download(output_path)
 
+        print(f"Downloaded {youtube.title} successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
+if __name__ == "__main__":
+    video_url = input("Enter the YouTube video URL: ")
+    output_folder = input("Enter the output folder path: ")
+
+    download_youtube_video(video_url, output_folder)
