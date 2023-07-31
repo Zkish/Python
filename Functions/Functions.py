eeecf4fd4f2b51cb36ba2a5a3ae754dc2d959f19
Zkish <zachary.kish@gmail.com>
2023-07-31 00:56:47 -0400

    added some useful functions to be called upon.

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,1 +1,18 @@
 # My Functions Page
+
+# removing duplicates
+def remove_duplicates(lst):
+    seen = set()
+    unique_list = []
+    for item in lst:
+        if item not in seen:
+            seen.add(item)
+            unique_list.append(item)
+    return unique_list
+
+# count occurences of elements in a list
+def count_elements(lst):
+    element_count = {}
+    for element in lst:
+        element_count[element] = element_count.get(element, 0) + 1
+    return element_count
