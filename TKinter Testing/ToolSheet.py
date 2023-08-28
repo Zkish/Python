6fe42191866b799b7e1d3d9d4ec73ae94b966aae
Zkish <zachary.kish@gmail.com>
2023-08-28 00:59:44 -0400

    adding things to the toolbox with tkinter.

--- TKinter Testing/ToolSheet.py
+++ TKinter Testing/ToolSheet.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+
 # Definitions
+
 # Calculator Definition
 def Calculator():
     selection = input("would you like to add, subtract, multiply, or divide? \n")
@@ -18,7 +20,7 @@ def Calculator():
         # multiplication
     elif selection == "multiply":
         print("welcome to multiply calculation")
-        
+
         # division
     elif selection == "divide":
         print("welcome to divide calculation")
@@ -29,6 +31,10 @@ def charLength():
     length2 = length.replace(" ", "")
     print(len(length2))
 
+# entry box Definition
+def outputName(event):
+    name = entry.get()
+    exitlabel.config(text="You Entered: " + name)
  
 
 #---------------------------------------------------------------
@@ -36,6 +42,7 @@ def charLength():
 # window form
 window = tk.Tk()
 
+# greeting lable
 greeting = tk.Label(text="Welcome to My Utility Application")
 greeting.pack()
 
@@ -46,6 +53,17 @@ calc.pack()
 # charLength button
 length = tk.Button(text="Find Length of String", command=charLength)
 length.pack()
+
+# creating an entry box that will jsut display what is entered into it
+entrylabel = tk.Label(text="Enter Your Name:")
+entrylabel.pack()
+
+entry = tk.Entry()
+entry.pack()
+entry.bind("<Return>", outputName)
+
+exitlabel = tk.Label(text="you entered: ")
+exitlabel.pack()
 
 # loop to run window form
 window.mainloop()
