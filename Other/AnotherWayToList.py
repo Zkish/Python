62305d295d177fafa30db2093ce74d2241765cd9
Zkish <zachary.kish@gmail.com>
2023-11-16 04:15:45 -0500

    adding some things.

---
+++ Other/AnotherWayToList.py
@@ -0,0 +1,13 @@
+# creating the list
+list = []
+i = 0
+while i < 10:
+    i += 1
+    list.append(input("Enter the item you want to put into the list:\n"))
+    continue
+
+# displaying the list
+for x in list:
+    print(x, "\n")
+    continue
+exit()
