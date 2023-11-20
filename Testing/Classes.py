607a947eb9b32c03e1201c09ff5d05b87378c558
Zkish <zachary.kish@gmail.com>
2023-11-20 03:23:41 -0500

    adding fucntions and classes to be called

---
+++ Testing/Classes.py
@@ -0,0 +1,6 @@
+# customer class
+class Customer:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
---
+++ Testing/Functions.py
@@ -0,0 +1,43 @@
+# My Functions Page
+
+# removing duplicates
+def remove_duplicates(lst):
+    seen = set()
+    unique_list = []
+    for item in lst:
+        if item not in seen:
+            seen.add(item)
+            unique_list.append(item)
+    return unique_list
+
+# count occurences of elements in a list
+def count_elements(lst):
+    element_count = {}
+    for element in lst:
+        element_count[element] = element_count.get(element, 0) + 1
+    return element_count
+
+# adding numbers
+def add_numbers(x, y):
+    result = x + y
+    return result
+
+# subtracting numbers
+def subtract_numbers(x, y):
+    result = x - y
+    return result
+
+# dividing numbers
+def divide_numbers(x, y):
+    result = x / y
+    return result
+
+# DataReader Function
+def datareader(file):
+    for x in file:
+        print(x)
+
+# Find Length of  a String
+def Find_length(input_string):
+    length = len(input_string.replace(" ", ""))
+    return length
